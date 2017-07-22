e981967b3586b62d2e0b301172388313f54ef055
Victor <vchipntus@outlook.com>
2017-07-21 23:29:48 -0300

    Wrapping more functions of sqlite. Handling exceptions.

--- tools/sqlite.py
+++ tools/sqlite.py
@@ -3,18 +3,26 @@
 import sqlite3
 
 
-class QueryError(Exception):
+class Error(Exception):
     '''
-    Exception raised errors in query.
+    Base class for exceptions
+    '''
+    pass
+
+
+class SqliteError(Error):
+    '''
+    Exception raised errors in the class.
 
     Attributes:
-        expression -- input expression in which error occurred
         message -- explanation of error
+        expression -- input expression in which error occurred
     '''
 
-    def __init__(self, expression, message):
-        self.expression = expression
+    def __init__(self, message, expression, *args):
         self.message = message
+        self.expression = expression
+        super(SqliteError, self).__init__(message, expression, *args)
 
 
 class Sqlite(object):
@@ -36,19 +44,8 @@ class Sqlite(object):
             f.close()
 
         self.conn = sqlite3.connect(db)
-
-    def execute(self, query=None, params=None):
-        '''
-        Execute a query on sqlite3
-        '''
-
-        if not query:
-            raise QueryError(query, "Empty query on execute function.")
-
-        # params can be None
-        cursor = self.conn.cursor()
-
-        cursor.execute(query, params)
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
 
     def commit(self):
         '''
@@ -60,4 +57,46 @@ class Sqlite(object):
         '''
         Close connection to the database
         '''
+        self.cursor.close()
         self.conn.close()
+
+    def execute(self, query=None, params=None):
+        '''
+        Execute a query on sqlite3
+        '''
+
+        if not query:
+            raise SqliteError("Empty query on execute function.",
+                              query)
+
+        # params can be None
+        try:
+            self.cursor.execute(query, params)
+        except sqlite3.DataError as err:
+            raise SqliteError("Data Error.", err)
+        except sqlite3.IntegrityError as err:
+            raise SqliteError("Integrity Error.", err)
+        except sqlite3.ProgrammingError as err:
+            raise SqliteError("Programming Error.", err)
+        except sqlite3.Warning as warn:
+            raise Warning("Sqlite Warning.", warn)
+        except sqlite3.Error as err:
+            raise SqliteError("Error.", err)
+
+    def fetchone(self):
+        '''
+        sqlite3 fetchone wrapper
+        '''
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        '''
+        sqlite3 fetchall wrapper
+        '''
+        return self.cursor.fetchall()
+
+    def lastrowid(self):
+        '''
+        sqlite3 lastrowid wrapper
+        '''
+        return self.cursor.lastrowid
